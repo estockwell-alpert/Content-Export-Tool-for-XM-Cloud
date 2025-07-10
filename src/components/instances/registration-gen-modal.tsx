dabc0c3d50b064458d9b98ed70eb20f55d05cba4
estockwellalpert <erica.stockwell-alpert@velir.com>
2025-07-10 14:51:16 -0400

    REGISTRATION - replace form with custom inputs

--- src/components/instances/registration-gen-modal.tsx
+++ src/components/instances/registration-gen-modal.tsx
@@ -1,23 +1,11 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { enumInstanceType, IInstance } from '@/models/IInstance';
-import { getAccessToken } from '@/services/sitecore/generateAccessToken';
 import { getXmCloudToken } from '@/services/sitecore/getXmCloudToken';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { ChangeEvent, useState } from 'react';
 import { z } from 'zod';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 
 const formSchema = z.object({
   name: z.string().min(2, { message: 'Name must be at least 2 characters' }),
@@ -39,51 +27,50 @@ interface InstanceRegistrationModalProps {
 }
 
 export const RegistrationGenModal = ({ open, onOpenChange, onSubmit }: InstanceRegistrationModalProps) => {
-  const form = useForm<FormValues>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: '',
-      identityServerUrl: '',
-      sitecoreUsername: '',
-      sitecorePassword: '',
-      clientId: '',
-      clientSecret: '',
-      graphQlEndpoint: '',
-      instanceType: enumInstanceType.auth,
-    },
-  });
+  const [instanceName, setInstanceName] = useState<string>('');
+  const [endpoint, setEndpoint] = useState<string>('');
+  const [clientid, setClientid] = useState<string>('');
+  const [clientsecret, setClientsecret] = useState<string>('');
 
   const [hasError, setHasError] = useState<boolean>(false);
 
-  const instanceType = form.watch('instanceType');
+  const handleInstanceName = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setInstanceName(inputValue);
+  };
+  const handleEndpoint = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setEndpoint(inputValue);
+  };
+  const handleClientId = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setClientid(inputValue);
+  };
+  const handleClientSecret = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setClientsecret(inputValue);
+  };
 
-  const handleSubmit = async (values: FormValues) => {
+  const handleSubmit = async () => {
     try {
       setHasError(false);
       let tokenResponse;
-      if (instanceType === enumInstanceType.auth) {
-        tokenResponse = await getXmCloudToken(values.clientId, values.clientSecret);
-      } else {
-        tokenResponse = await getAccessToken(
-          values.identityServerUrl,
-          values.sitecoreUsername,
-          values.sitecorePassword,
-          values.clientId,
-          values.clientSecret
-        );
-      }
+      tokenResponse = await getXmCloudToken(clientid, clientid);
 
       onSubmit({
-        name: values.name,
-        graphQlEndpoint: values.graphQlEndpoint,
-        instanceType: values.instanceType,
+        name: instanceName,
+        graphQlEndpoint: endpoint,
+        instanceType: enumInstanceType.auth,
         apiToken: tokenResponse.access_token,
         expiration: new Date(Date.now() + tokenResponse.expires_in * 1000).toISOString(),
-        clientId: values.clientId,
-        clientSecret: values.clientSecret,
+        clientId: clientid,
+        clientSecret: clientsecret,
       });
 
-      form.reset();
+      setInstanceName('');
+      setEndpoint('');
+      setClientid('');
+      setClientsecret('');
     } catch (error) {
       console.error(error);
       setHasError(true);
@@ -95,104 +82,56 @@ export const RegistrationGenModal = ({ open, onOpenChange, onSubmit }: InstanceR
       <DialogContent className="sm:max-w-[600px]">
         <DialogHeader>
           <DialogTitle>Register New Instance</DialogTitle>
-          <DialogDescription>Fill in the details to register a new instance to your configuration.</DialogDescription>
+          <DialogDescription>
+            Fill in the details to register a new XM Cloud Authoring instance to your configuration.
+          </DialogDescription>
         </DialogHeader>
 
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="instanceType"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Instance Type</FormLabel>
-                  <Select onValueChange={field.onChange} defaultValue={field.value}>
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select instance type" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value={enumInstanceType.auth}>Sitecore XM Cloud</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )}
+        <div className="space-y-6">
+          <div className="grid gap-2">
+            <label className="flex items-center gap-2 text-sm leading-none font-medium">Instance Name</label>
+            <Input
+              value={instanceName}
+              onChange={handleInstanceName}
+              placeholder="My XM Cloud Site"
+              className={'font-mono text-sm'}
             />
-
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Instance Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Production Server" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+          </div>
+          <div className="grid gap-2">
+            <label className="flex items-center gap-2 text-sm leading-none font-medium">GraphQL Endpoint</label>
+            <Input
+              value={endpoint}
+              onChange={handleEndpoint}
+              placeholder="https://xmc-myapp.sitecorecloud.io/sitecore/api/authoring/graphql/v1"
+              className={'font-mono text-sm'}
             />
-
-            <FormField
-              control={form.control}
-              name="graphQlEndpoint"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>GraphQL Endpoint</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="https://xmc-mysite.sitecorecloud.io/sitecore/api/authoring/graphql/v1"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+          </div>
+          <div className="grid gap-2">
+            <label className="flex items-center gap-2 text-sm leading-none font-medium">Client ID</label>
+            <Input
+              value={clientid}
+              onChange={handleClientId}
+              placeholder="yj92f9ar3lep6sQeiWOeriKsRPH2nEXE"
+              className={'font-mono text-sm'}
             />
-
-            <FormField
-              control={form.control}
-              name="clientId"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Client ID</FormLabel>
-                  <FormControl>
-                    <Input placeholder="client-id" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+          </div>
+          <div className="grid gap-2">
+            <label className="flex items-center gap-2 text-sm leading-none font-medium">Client Secret</label>
+            <Input
+              value={clientsecret}
+              type="password"
+              onChange={handleClientSecret}
+              placeholder="Bl9OuReXSQ...."
+              className={'font-mono text-sm'}
             />
-
-            <FormField
-              control={form.control}
-              name="clientSecret"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Client Secret</FormLabel>
-                  <FormControl>
-                    <Input type="password" placeholder="client-secret" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            {hasError && (
-              <DialogDescription className="error">
-                An error occurred in the token request. Things to check: username and password; Client ID and Client
-                Secret; Identity Server URL is publicly accessible; Token request works in Postman
-              </DialogDescription>
-            )}
-            <DialogFooter>
-              <Button type="button" variant="outline" onClick={() => onOpenChange(false)}>
-                Cancel
-              </Button>
-              <Button type="submit">Register Instance</Button>
-            </DialogFooter>
-          </form>
-        </Form>
+          </div>
+          <div className="flex flex-col-reverse gap-2 sm:flex-row sm:justify-end">
+            <Button type="button" variant="outline" onClick={() => onOpenChange(false)}>
+              Cancel
+            </Button>
+            <Button onClick={() => handleSubmit()}>Register Instance</Button>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
